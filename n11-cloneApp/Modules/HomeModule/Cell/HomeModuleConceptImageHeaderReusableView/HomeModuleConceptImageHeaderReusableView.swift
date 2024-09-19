007f684d604c8d7ea0173a76194392edd5381943
Bünyamin Mete <kaanmetee5@gmail.com>
2024-09-19 16:01:03 +0300

    Missing file added.

--- n11-cloneApp/Modules/HomeModule/Cell/HomeModuleConceptImageHeaderReusableView/HomeModuleConceptImageHeaderReusableView.swift
+++ n11-cloneApp/Modules/HomeModule/Cell/HomeModuleConceptImageHeaderReusableView/HomeModuleConceptImageHeaderReusableView.swift
@@ -9,24 +9,24 @@ import UIKit
 
 class HomeModuleConceptImageHeaderReusableView: UICollectionReusableView {
     static let headerIdentifier = "HomeModuleConceptImageHeaderReusableView"
-    
+
     private let conceptImageView = UIImageView()
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
     }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setupView()
     }
-    
+
     private func setupView() {
         // ImageView'i ekle
         conceptImageView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(conceptImageView)
-        
+
         // ImageView'un tüm kenarları 0 olacak şekilde constraints ayarla
         NSLayoutConstraint.activate([
             conceptImageView.topAnchor.constraint(equalTo: topAnchor),
@@ -34,15 +34,14 @@ class HomeModuleConceptImageHeaderReusableView: UICollectionReusableView {
             conceptImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
             conceptImageView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
-        
+
         // ImageView'un içeriğini kesip göstermesi için içerik modunu ayarla
         conceptImageView.contentMode = .scaleToFill
         conceptImageView.clipsToBounds = true
     }
-    
+
     func configure(with image: UIImage?) {
         conceptImageView.image = image
     }
 }
-
 
